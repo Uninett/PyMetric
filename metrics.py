ee42f3a18648a53ffe679cc3838f4ca5914e375c
Morten Knutsen <morten.knutsen@uninett.no>
2010-03-22 15:40:02 +0100

    Bump to 0.9.1

--- metrics.py
+++ metrics.py
@@ -19,7 +19,7 @@ its job regardless of the routing protocol used (as long as it
 is a LSP)
 
 """
-__version__   = "1.0-pre (git/master)"
+__version__   = "0.9.1"
 __author__    = """Morten Knutsen (morten.knutsen@uninett.no)"""
 __copyright__ = """Copyright (C) 2009-2010
 Morten Knutsen <morten.knutsen@uninett.no>
