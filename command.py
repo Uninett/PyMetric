d041d93456c12c00e7ae4120756e883426fe8cb6
Morten Knutsen <morten.knutsen@uninett.no>
2015-09-04 14:41:52 +0200

    Make tab completion compatible with OSX readline interface.

--- command.py
+++ command.py
@@ -21,6 +21,11 @@ class MetricShell(Cmd):
       except IOError:
          pass
 
+      if 'libedit' in readline.__doc__:
+         readline.parse_and_bind("bind ^I rl_complete")
+      else:
+         readline.parse_and_bind("tab: complete")
+
       self.config = Config()
 
       readline.set_completer_delims(" ")
